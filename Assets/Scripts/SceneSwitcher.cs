9df6c0bcbb558fc5a3a776804d082cabbe686de8
MaikelHendrikx1 <maikel.hendrikx@outlook.com>
2024-02-20 11:33:54 +0100

    Add TMPro, scene management, and some scenes
    - Added TextMeshPro
    - Added a simple scene management system (to switch between scenes)
    - Added some content to the mainscreen, as well as one of the rulescreens (screen where the rules for one of the games is displayed)

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToScene(Object scene)
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+}
